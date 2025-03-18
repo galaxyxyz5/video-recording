db4ec03ef2ef7fc36b141e36ba26f35480739da8
galaxyxyz5 <galaxyxyz5@outlook.como>
2025-03-18 04:11:34 -0400

    #1 conf:// add cors option

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,14 @@ import recordingRouter from './routes/recording.routes';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:4200', 'https://video-recording-app.app.genez.io/'],
+  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api', recordingRouter);
